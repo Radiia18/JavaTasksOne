8386f5298f11e4a43e30ce3c06efc9e472e9b71d
Student <student@gmail.com>
2026-09-24 15:50:27 +0600

    first commit

---
+++ src/TaskSix.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TaskSix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.nextLine();
+
+        System.out.println(n.charAt(n.length() - 1));
+    }
+}
