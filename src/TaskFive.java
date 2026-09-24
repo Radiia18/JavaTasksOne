1edf3822aea3df805752f601897f82c89fc95f2e
Student <student@gmail.com>
2026-09-24 15:46:51 +0600

    first commit

---
+++ src/TaskFive.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TaskFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int v = sc.nextInt();
+        int t = sc.nextInt();
+
+        int position = (v * t) % 109;
+
+        if (position < 0) {
+            position += 109;
+        }
+
+        System.out.println(position);
+    }
+}
+
